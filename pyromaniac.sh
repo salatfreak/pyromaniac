f0e64b0fe22ec05b6573f03b96a99a13a849d900
the-dipsy <the-dipsy@github>
2024-02-11 18:21:29 +0100

    feat: add bash script to wrap podman command

---
+++ pyromaniac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[[ -t 0 ]] && input='-it' || input='-i'
+
+readonly DIR="$(realpath "$(dirname "$0")")"
+exec podman run --rm "$input" \
+  -v "$DIR/pyromaniac:/src/pyromaniac:ro" \
+  -v "pyromaniac-cache:/data/cache" \
+  -v "pyromaniac-secrets:/data/secrets" \
+  -v ".:/spec:ro" \
+  -p 8000:8000 \
+  ghcr.io/salatfreak/pyromaniac "$@"
