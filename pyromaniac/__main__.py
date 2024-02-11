6334217baf76961ab0116eb3da8f85fc456ec9aa
the-dipsy <the-dipsy@github>
2024-02-11 19:04:13 +0100

    feat: add reload logic and output mode handling

--- pyromaniac/__main__.py
+++ pyromaniac/__main__.py
@@ -3,4 +3,20 @@ from .__init__ import compile
 
 args = parse()
 
-print(compile(args.input.read_text(), args.address, args.auth))
+
+def ignition():
+    # only reload source if not from a character device (like standard input)
+    global source
+    if 'source' not in globals() or not args.input.is_char_device():
+        source = args.input.read_text()
+
+    return compile(source, args.address, args.auth)
+
+
+match args.mode:
+    case 'ign':
+        print(ignition())
+    case 'iso':
+        raise NotImplementedError()
+    case 'serve':
+        raise NotImplementedError()
