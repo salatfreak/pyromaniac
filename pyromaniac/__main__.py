86c55816bb58fae9feafb056a9a1a18712776b37
the-dipsy <the-dipsy@github>
2024-02-11 18:12:24 +0100

    feat: add scaffold for config compiler

--- pyromaniac/__main__.py
+++ pyromaniac/__main__.py
@@ -1,3 +1,6 @@
 from .args import parse
+from .__init__ import compile
 
 args = parse()
+
+print(compile(args.input.read_text(), args.address, args.auth))
---
+++ pyromaniac/compiler/__init__.py
@@ -0,0 +1,28 @@
+from typing import Self
+from pathlib import PosixPath as Path
+
+
+class Compiler:
+    """Pyromaniac config compiler."""
+
+    @classmethod
+    def create(cls, path: Path) -> Self:
+        """Create new config compiler.
+
+        :param path: path to component library
+        :returns: created compiler
+        """
+        return cls()
+
+    def compile(
+        self, source: str,
+        address: tuple[str, str, int], auth: str | None = None,
+    ) -> str:
+        """Compile config to ignition.
+
+        :param source: pyromaniac config source text
+        :param address: scheme, host and port for encryption secret requests
+        :param auth: basic auth credentials for encryption secret reqeusts
+        :returns: compiled ignition config
+        """
+        return "{}"
