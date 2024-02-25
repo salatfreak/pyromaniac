6a81d0938e73d68159f8bc3651b1fc8a156374b0
the-dipsy <the-dipsy@github>
2024-02-25 23:36:48 +0100

    refactor: move signature error before argument error

--- pyromaniac/compiler/code/errors.py
+++ pyromaniac/compiler/code/errors.py
@@ -10,6 +10,12 @@ class InvalidDocstringError(CodeError):
     pass
 
 
+class InvalidSignatureError(CodeError):
+    @classmethod
+    def invalid_type(cls, type: str) -> Self:
+        return cls(f"unspported type specification {type}")
+
+
 class InvalidArgumentError(CodeError):
     @classmethod
     def wrong_type(cls, value: Any, type: str) -> Self:
@@ -18,9 +24,3 @@ class InvalidArgumentError(CodeError):
     @classmethod
     def wrong_count(cls, value: Any, count: int) -> Self:
         return cls(f"argument {value} should have {count} elements")
-
-
-class InvalidSignatureError(CodeError):
-    @classmethod
-    def invalid_type(cls, type: str) -> Self:
-        return cls(f"unspported type specification {type}")
