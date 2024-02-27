47d413d69fce5155a9c46fd15b9e1753589975bc
the-dipsy <the-dipsy@github>
2024-02-27 20:29:02 +0100

    fix: add missing type annotations to ISO error constructors

--- pyromaniac/iso/errors.py
+++ pyromaniac/iso/errors.py
@@ -6,10 +6,10 @@ class IsoError(PyromaniacError):
 
 
 class DownloadError(IsoError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         self.message = message
 
 
 class CustomizeError(IsoError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         self.message = message
