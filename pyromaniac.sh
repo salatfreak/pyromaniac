90aeff13f6a53ebd7fda28957a8425d8dfb08597
the-dipsy <the-dipsy@github>
2024-02-28 12:15:16 +0100

    fix: disable SELinux labels in podman container

--- pyromaniac.sh
+++ pyromaniac.sh
@@ -4,6 +4,7 @@
 
 readonly DIR="$(realpath "$(dirname "$0")")"
 exec podman run --rm "$input" \
+  --security-opt "label=disable" \
   -v "$DIR/pyromaniac:/src/pyromaniac:ro" \
   -v "pyromaniac-cache:/data/cache" \
   -v "pyromaniac-secrets:/data/secrets" \
