f50982fe404587c261abd80cfdee135e60317842
the-dipsy <the-dipsy@github>
2024-02-28 14:14:46 +0100

    feat: add ability to pass args for component to compile function

--- pyromaniac/compile.py
+++ pyromaniac/compile.py
@@ -1,3 +1,4 @@
+from typing import Any
 from pathlib import PosixPath as Path
 
 from .compiler import Compiler
@@ -5,12 +6,16 @@ from .compiler import Compiler
 
 def compile(
     source: str, address: tuple[str, str, int], auth: str | None = None,
+    args: list = [], kwargs: dict[str, Any] = {},
 ) -> str:
     """Compile config to ingnition.
 
     :param source: pyromaniac config source text
     :param address: scheme, host and port for encryption secret requests
     :param auth: basic auth credentials for encryption secret reqeusts
+    :param args: positional arguments to pass to the component
+    :param kwargs: keyword arguments to pass to the component
     :returns: compiled ignition config
     """
-    return Compiler.create(Path(".")).compile(source, address, auth)
+    compiler = Compiler.create(Path("."))
+    return compiler.compile(source, address, auth, args, kwargs)
