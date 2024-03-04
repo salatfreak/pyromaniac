0bf2b731f5e10865fac2080626be953381019db4
the-dipsy <the-dipsy@github>
2024-03-04 22:47:31 +0100

    test: add tests for *contents* stdlib component

---
+++ tests/stdlib/test_contents.py
@@ -0,0 +1,39 @@
+from pathlib import PosixPath as Path
+from pyromaniac.compiler.url import URL
+
+from .base import TestCase
+
+
+class TestMagic(TestCase):
+    comp = 'contents'
+
+    def test_minimal(self):
+        path, url = Path("file.txt"), URL("https://example.com/")
+        self.assertEqual(self.call("foo"), {"inline": "foo"})
+        self.assertEqual(self.call(path), {"local": path})
+        self.assertEqual(self.call(url), {"source": url})
+        self.assertEqual(self.call({'inline': "foo"}), {'inline': "foo"})
+
+    def test_complex(self):
+        result = self.call({
+            'source': "https://example.com/",
+            'http_headers': [{'name': "foo", 'value': "bar"}],
+        }, {
+            "foo": "baz",
+            "qux": "quux",
+        }, http_headers=[
+            {'name': "foo", 'value': "corge"},
+            {'name': "fred", 'value': "waldo"},
+        ], compression=None)
+
+        self.assertEqual(
+            set(result.keys()), set(['source', 'http_headers', 'compression'])
+        )
+        self.assertEqual(result['source'], "https://example.com/")
+        self.assertEqual(result['compression'], None)
+        headers = sorted(result['http_headers'], key=lambda d: d['name'])
+        self.assertEqual(headers, [
+            {'name': "foo", 'value': "bar"},
+            {'name': "fred", 'value': "waldo"},
+            {'name': "qux", 'value': "quux"},
+        ])
