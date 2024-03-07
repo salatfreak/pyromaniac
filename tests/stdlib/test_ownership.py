d3787aa773c047468a109ccd300f3df388a997b0
the-dipsy <the-dipsy@github>
2024-03-07 12:35:53 +0100

    test: add tests for ownership stdlib component

---
+++ tests/stdlib/test_ownership.py
@@ -0,0 +1,20 @@
+from .base import TestCase
+
+
+class TestOwnership(TestCase):
+    comp = 'ownership'
+
+    def test_defaults(self):
+        self.assertEqual(self.call(), {})
+        self.assertEqual(self.call("core"), {
+            'user': {'name': "core"}, 'group': {'name': "core"},
+        })
+        self.assertEqual(self.call("core", None), {'user': {'name': "core"}})
+
+    def test_types(self):
+        self.assertEqual(self.call(1001, "alice"), {
+            'user': {'id': 1001}, 'group': {'name': "alice"},
+        })
+        self.assertEqual(self.call("bob", 1002), {
+            'user': {'name': "bob"}, 'group': {'id': 1002},
+        })
