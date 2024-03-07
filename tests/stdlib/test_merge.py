f48e69197802929f216c89ccd17496027b34aa74
the-dipsy <the-dipsy@github>
2024-03-07 14:04:39 +0100

    test: adapt merge component tests to generation of only array field

--- tests/stdlib/test_merge.py
+++ tests/stdlib/test_merge.py
@@ -10,38 +10,27 @@ class TestMerge(TestCase):
 
     def test_types(self):
         result = self.call("{}", Path("/foo.txt"), URL("https://bar.com/"))
-        self.assertEqual(result, {
-            'ignition.config.merge': [
-                {'inline': "{}"},
-                {'local': Path("/foo.txt")},
-                {'source': URL("https://bar.com/")},
-            ],
-        })
+        self.assertEqual(result, [
+            {'inline': "{}"},
+            {'local': Path("/foo.txt")},
+            {'source': URL("https://bar.com/")},
+        ])
 
     def test_render(self):
         result = self.call("{}", {'storage.files[0].path': "/foo.txt"})
-        merge = result['ignition.config.merge']
-        self.assertEqual(merge[0], {'inline': "{}"})
-        self.assertEqual(loads(merge[1]['inline'])['storage'], {
+        self.assertEqual(result[0], {'inline': "{}"})
+        self.assertEqual(loads(result[1]['inline'])['storage'], {
             'files': [{'path': "/foo.txt"}],
         })
 
     def test_headers(self):
         headers = {"Authorization": "Basic secret"}
         result = self.call("{}", URL("https://bar.com/"), headers=headers)
-        self.assertEqual(result, {
-            'ignition.config.merge': [{
-                'inline': "{}",
-            }, {
-                'source': URL("https://bar.com/"),
-                'http_headers': [
-                    {'name': "Authorization", 'value': "Basic secret"},
-                ],
-            }],
-        })
-
-    def test_fields(self):
-        self.assertEqual(self.call("{}", foo="bar"), {
-            'ignition.config.merge': [{'inline': "{}"}],
-            'foo': "bar",
-        })
+        self.assertEqual(result, [{
+            'inline': "{}",
+        }, {
+            'source': URL("https://bar.com/"),
+            'http_headers': [
+                {'name': "Authorization", 'value': "Basic secret"},
+            ],
+        }])
