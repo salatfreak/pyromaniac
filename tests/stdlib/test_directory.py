4cf70ba8a7d8ae04bf1fc4bf376ce2b6c6d57486
the-dipsy <the-dipsy@github>
2024-03-07 15:58:12 +0100

    test: add tests for directory stdlib component

---
+++ tests/stdlib/test_directory.py
@@ -0,0 +1,21 @@
+from pathlib import PosixPath as Path
+
+from .base import TestCase
+
+
+class TestLink(TestCase):
+    comp = 'directory'
+
+    def test_minimal(self):
+        self.assertEqual(self.call("/foo"), {'path': Path("/foo")})
+
+    def test_complex(self):
+        self.assertEqual(
+            self.call(
+                "foo", "core", None, mode=0o550,
+            ), {
+                'path': Path("/home/core/foo"),
+                'user': {'name': "core"},
+                'mode': 0o550,
+            }
+        )
