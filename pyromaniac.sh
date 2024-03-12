a46b8d2b619356d0df1a741f383207fa0469768e
the-dipsy <the-dipsy@github>
2024-03-12 11:51:43 +0100

    feat: add handling of ports and volumes to Bash script
    Allows to control volume mounts with --debug and --no-cache flags and
    control of the HTTP(S) port via the --address parameter by parsing and
    modifying the command line parameters.
    
    Switches to fixed version number in container image reference.

--- pyromaniac.sh
+++ pyromaniac.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
-[[ -t 0 ]] && input='-it' || input='-i'
+# extract server parameters from arguments
+serve=false port=8000 cache=true debug=false
+declare -a args=("$@")
+for (( i = 0; i < $#; ++i )); do
+  case "${args[i]}" in
+    -- | -p | --pretty | -s | --strict | --help \
+      | --iso-arch=* | --iso-net=* | --iso-disk=* | --auth=* \
+      | --iso-raw-force | --iso-raw-help | --iso-raw-*=* ) ;;
+    --iso-arch | --iso-net | --iso-disk | --auth | --iso-raw-* ) let ++i;;
+    --serve ) serve=true;;
+    --iso ) serve=false;;
+    --address | --address=*)
+      [[ "${args[i]}" == *=* ]] && addr="${args[i]#*=}" || addr="${args[++i]}"
+      [[ ! "${addr%/}" =~ :([0-9]+)$ ]] || port="${BASH_REMATCH[1]}"
+      ;;
+    --no-cache ) cache=false; unset args[i];;
+    --debug ) debug=true; unset args[i];;
+    * ) break;;
+  esac
+  [[ "${args[i]}" != '--' ]] || break
+done
 
-readonly DIR="$(realpath "$(dirname "$0")")"
-exec podman run --rm "$input" \
-  --security-opt 'label=disable' \
-  -v "$DIR/pyromaniac:/src/pyromaniac:ro" \
-  -v "$DIR/stdlib:/usr/local/lib/pyromaniac:ro" \
-  -v 'pyromaniac-cache:/data/cache' \
-  -v 'pyromaniac-secrets:/data/secrets' \
-  -v '.:/spec:ro' \
-  -p 8000:8000 \
-  ghcr.io/salatfreak/pyromaniac "$@"
+# collect extra arguments
+declare -a params=()
+if $cache; then
+  params+=(--volume 'pyromaniac-cache:/data/cache')
+  params+=(--volume 'pyromaniac-secrets:/data/secrets')
+fi
+if $debug; then
+  dir="$(realpath "$(dirname "$0")")"
+  params+=(--volume "$dir/pyromaniac:/src/pyromaniac:ro")
+  params+=(--volume "$dir/stdlib:/usr/local/lib/pyromaniac:ro")
+fi
+if [[ -t 0 ]]; then params+=(--tty); fi
+if $serve; then params+=(--publish "$port:8000"); fi
+
+# run pyromaniac using podman
+exec podman run \
+  --rm --interactive --security-opt 'label=disable' \
+  --volume '.:/spec:ro' \
+  "${params[@]}" \
+  ghcr.io/salatfreak/pyromaniac:0.1 "${args[@]}"
