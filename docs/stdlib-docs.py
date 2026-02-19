2605b70ae740b1f3e70b17eb9f0a23db164bde10
the-dipsy <the-dipsy@github>
2026-02-19 21:07:12 +0100

    docs: fix wrong comp name in stdlib docs script

--- docs/stdlib-docs.py
+++ docs/stdlib-docs.py
@@ -21,7 +21,7 @@ ORDER = [
     'merge',
     'load', 'load.json', 'load.yaml', 'load.toml', 'magic',
     'file', 'link', 'directory', 'directories', 'tree', 'storage',
-    'contents', 'contents.parse', 'ownership', 'python.merge',
+    'contents', 'contents.parse', 'ownership', 'py.merge',
 ]
 STDLIB = Path("/", "usr", "local", "lib", "pyromaniac", "std")
 
