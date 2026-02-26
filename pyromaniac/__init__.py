a9f4e07c9554f4a657c5c089d587d807ca39af7e
the-dipsy <the-dipsy@github>
2026-02-26 16:32:39 +0100

    refactor: remove expand and butane from global ctx
    The "expand" and "butane" functions now are not part of the component
    execution context anymore by default. They can be imported from the
    pyromaniac package.

--- pyromaniac/__init__.py
+++ pyromaniac/__init__.py
@@ -1,5 +1,7 @@
 from .errors import PyromaniacError
 from .remote import Remote
 from .compile import compile
+from .compiler.expand import expand
+from .compiler.butane import butane
 
-__all__ = [compile, Remote, PyromaniacError]
+__all__ = [compile, butane, expand, Remote, PyromaniacError]
--- pyromaniac/compiler/context.py
+++ pyromaniac/compiler/context.py
@@ -2,17 +2,11 @@ from typing import Any, TYPE_CHECKING
 from pathlib import PosixPath as Path
 
 from .url import URL
-from .expand import expand
-from .butane import butane
 
 if TYPE_CHECKING:
     from .library import Library, View
 
-CONTEXT = {
-    'Any': Any, 'Path': Path, 'URL': URL,
-    'butane': butane, 'expand': expand,
-    'GLOBAL': {}
-}
+CONTEXT = {'Any': Any, 'Path': Path, 'URL': URL, 'GLOBAL': {}}
 
 
 def context(lib: 'Library', view: 'View', **kwargs) -> dict:
