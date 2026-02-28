30ff4c764f5ae1c6592ea188ea170d6a8f9771e6
the-dipsy <the-dipsy@github>
2026-02-28 11:11:49 +0100

    test: adapt to updated std.tree component

--- tests/stdlib/test_tree.py
+++ tests/stdlib/test_tree.py
@@ -1,53 +1,23 @@
 from pathlib import PosixPath as Path
-from tarfile import TarFile
 
-from ..temp import dir
 from .base import TestCase
+
+target = 'storage.trees[]'
 
 
 class TestTree(TestCase):
     comp = 'std.tree'
 
-    @dir
-    def test_empty(self, tmp: Path):
-        self.assertEqual(self.call("/dir", tmp), {
-            'directories': [
-                {'path': Path("/dir"), '__for__': 'storage.directories[]'}
-            ],
-            'files': [],
-            'links': [],
-            '__for__': 'storage',
+    def test_minimal(self):
+        self.assertEqual(self.call("/foo", "bar"), {
+            'path': Path("/foo"), 'local': Path("bar"), '__for__': target,
         })
 
-    @dir
-    def test_full(self, tmp: Path):
-        # unpack archive in temporary directory
-        tar = TarFile.open(self.lib.view() / "tree.tar")
-        tar.extractall(tmp, filter='tar')
-        tar.close()
-        local = tmp.joinpath("tree")
-
-        # test construction of tree
-        common = {'user': {'name': "alice"}, 'overwrite': True}
-        self.assertEqual(self.call("dir", local, "alice", None, True, True), {
-            'directories': [{
-                'path': Path("/home/alice/dir"), 'mode': 0o755, **common,
-                '__for__': 'storage.directories[]',
-            }, {
-                'path': Path("/home/alice/dir/foo"), 'mode': 0o510, **common,
-                '__for__': 'storage.directories[]',
-            }],
-            'files': [{
-                'path': Path("/home/alice/dir/foo/bar.sh"),
-                'contents': {'local': local.joinpath("foo/bar.sh")},
-                'mode': 0o755, **common,
-                '__for__': 'storage.files[]',
-            }],
-            'links': [{
-                'path': Path("/home/alice/dir/bar"),
-                'target': Path("foo/bar.sh"),
-                'mode': 0o777, **common,
-                '__for__': 'storage.links[]',
-            }],
-            '__for__': 'storage',
+    def test_complex(self):
+        self.assertEqual(self.call("foo", "bar", "core", None, dir_mode=0o555), {
+            'path': Path("/home/core/foo"),
+            'local': Path("bar"),
+            'user': {'name': "core"},
+            'dir_mode': 0o555,
+            '__for__': target,
         })
